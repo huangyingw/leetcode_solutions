d525a59a4bcdf1a7ca75c9968348a47f8b1ec3e1
Ying Huang <huangyingw@gmail.com>
2020-01-17 21:00:26 -0800

    n

---
+++ submit.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+find ~/.lc -type f -name problems.json -delete
+find . -type f -name "[0-9]*.py" | while read ss
+do
+    ~/loadrc/pythonrc/remove_comments.py "$ss"
+    mv -fv "$ss.strip" "$ss"
+    sed -i.bak '/print.*(/d;/^_author_/d;/__main__/d;/ = Solution()/d;/^_project_/d;/\bprint\b/d;s/#--//g;s/##//g;/^$/d;/^\s*$/d' "$ss"
+    autopep8 --in-place "$ss"
+    leetcode submit -d "$ss"
+done
